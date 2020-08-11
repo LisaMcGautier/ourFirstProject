6aa71674923236c6a018e6b2eb26ebe32f758485
Lisa McDonald <nixxxxer@aol.com>
2020-08-10 21:58:54 -0400

    Restaurants Display to local-shop

--- lisa/script.js
+++ lisa/script.js
@@ -2,38 +2,116 @@ $(document).ready(function () {
     $(".sidenav").sidenav();
     $(".modal").modal();
 
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://thefork.p.rapidapi.com/locations/list?google_place_id=ChIJEcHIDqKw2YgRZU-t3XHylv8",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "thefork.p.rapidapi.com",
-            "x-rapidapi-key": "a574a25ea2msh5e7d0b227f558d8p11749fjsn112a0fba655d"
-        }
+    // Modal to open on page load?  save location preference (google geolocate or zip code)
+    //  How to close the modal pop-up box??
+
+    // Add event listener zip code submit
+    // OR change to dropdown with subzones
+
+    // Google maps location GLOBAL variables latitude and longitude of current position 
+    // OR latitude and longitude of zip code
+
+    var map, infoWindow;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 10
+      });
+      infoWindow = new google.maps.InfoWindow;
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          console.log(pos);
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
     }
-    $.ajax(settings).done(function (response) {
-        console.log(response);
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+          'Error: The Geolocation service failed.' :
+          'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+    }
 
+    // save lat and lon in GLOBAL variables
 
-    });
-
+    // Zomato search 
+    //  SEARCH: REQUIRES ENTITY ID & ENTITY TYPE; 14,000+ RESULTS
+    //  EXAMPLE SHOWN = lat + lon + radius(meters) + cuisines + sort (distance)
     $.ajax({
         dataType: "json",
-
-        //  API call for Miami (entity_id=291)
-        //  entity_type=city
-        //  cuisines=Latin%20American
-        url: "https://developers.zomato.com/api/v2.1/search?entity_id=291&entity_type=city&cuisines=Latin%20American",
+        url: "https://developers.zomato.com/api/v2.1/search?lat=25.912989999999997&lon=-80.3209667&radius=5000&cuisines=Latin%20American&sort=real_distance",
         method: "GET",
-
-        //  API call requires header key and value
         headers: { "user-key": "6e5be356cc4a96733158bee5d1d847e7" }
     }).then(function (response) {
 
         console.log(response);
+        // console.log(response.restaurants[0]);
+        // console.log(response.restaurants[0].restaurant);
+        console.log(response.restaurants[0].restaurant.name);
+        console.log(response.restaurants[0].restaurant.R.res_id);
+
+        // console.log(response.restaurants[1]);
+        // console.log(response.restaurants[1].restaurant);
+        console.log(response.restaurants[1].restaurant.name);
+        console.log(response.restaurants[1].restaurant.R.res_id);
+
+        // console.log(response.restaurants[2]);
+        // console.log(response.restaurants[2].restaurant);
+        console.log(response.restaurants[2].restaurant.name);
+        console.log(response.restaurants[2].restaurant.R.res_id);
+
+        // console.log(response.restaurants[3]);
+        // console.log(response.restaurants[3].restaurant);
+        console.log(response.restaurants[3].restaurant.name);
+        console.log(response.restaurants[3].restaurant.R.res_id);
+
+        // console.log(response.restaurants[4]);
+        // console.log(response.restaurants[4].restaurant);
+        console.log(response.restaurants[4].restaurant.name);
+        console.log(response.restaurants[4].restaurant.R.res_id);
+
+        // $("#local-shop").empty();
+
+        for (var i = 0; i < 5; i++) {
+
+            var restaurantID = response.restaurants[i].R.res_id;
+        
+            // Needs to be a button or clickable element
+            var restaurantName = $("<p>");
+            restaurantName.text(response.restaurants[i].restaurant.name);
+            $("#local-shop").append(restaurantName);
+
+            // Add event listener to restaurantName
+            // materialize collapsible ??
+
+            // In order to create API search for menu
+        }
 
     });
+
+
+
+
+
+    
+
+
 
 
 })
