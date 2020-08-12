8e9161d6ddfc38d1494da315fa3e08587d58e3d4
Lisa McDonald <nixxxxer@aol.com>
2020-08-11 21:31:20 -0400

    Simplify HTML

--- lisa/script.js
+++ lisa/script.js
@@ -1,54 +1,41 @@
 $(document).ready(function () {
-    $(".sidenav").sidenav();
-    $(".modal").modal();
+    // $(".sidenav").sidenav();
+
 
     // Modal to open on page load?  save location preference (google geolocate or zip code)
     //  How to close the modal pop-up box??
 
+    // HTML5 geolocation pop-up message
+
+    var x = document.getElementById("demo");
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            // Modal : please enter your zip code
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    };
+
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
+    };
+
+
+
     // Add event listener zip code submit
+        $(".searchBtn").on("click", function () {
+            var thisZip = $("#zip-input").val();
+            console.log(thisZip);
+        
+        });
+
     // OR change to dropdown with subzones
 
     // Google maps location GLOBAL variables latitude and longitude of current position 
     // OR latitude and longitude of zip code
-
-    var map, infoWindow;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 10
-      });
-      infoWindow = new google.maps.InfoWindow;
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          console.log(pos);
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('Location found.');
-          infoWindow.open(map);
-          map.setCenter(pos);
-        }, function() {
-          handleLocationError(true, infoWindow, map.getCenter());
-        });
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-      }
-    }
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-          'Error: The Geolocation service failed.' :
-          'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(map);
-    }
-
-    // save lat and lon in GLOBAL variables
+    // save lat and lon in GLOBALvariables
 
     // Zomato search 
     //  SEARCH: REQUIRES ENTITY ID & ENTITY TYPE; 14,000+ RESULTS
@@ -61,28 +48,19 @@ $(document).ready(function () {
     }).then(function (response) {
 
         console.log(response);
-        // console.log(response.restaurants[0]);
-        // console.log(response.restaurants[0].restaurant);
+
         console.log(response.restaurants[0].restaurant.name);
         console.log(response.restaurants[0].restaurant.R.res_id);
 
-        // console.log(response.restaurants[1]);
-        // console.log(response.restaurants[1].restaurant);
         console.log(response.restaurants[1].restaurant.name);
         console.log(response.restaurants[1].restaurant.R.res_id);
 
-        // console.log(response.restaurants[2]);
-        // console.log(response.restaurants[2].restaurant);
         console.log(response.restaurants[2].restaurant.name);
         console.log(response.restaurants[2].restaurant.R.res_id);
 
-        // console.log(response.restaurants[3]);
-        // console.log(response.restaurants[3].restaurant);
         console.log(response.restaurants[3].restaurant.name);
         console.log(response.restaurants[3].restaurant.R.res_id);
 
-        // console.log(response.restaurants[4]);
-        // console.log(response.restaurants[4].restaurant);
         console.log(response.restaurants[4].restaurant.name);
         console.log(response.restaurants[4].restaurant.R.res_id);
 
@@ -90,7 +68,7 @@ $(document).ready(function () {
 
         for (var i = 0; i < 5; i++) {
 
-            var restaurantID = response.restaurants[i].R.res_id;
+            // var restaurantID = response.restaurants[i].R.res_id;
         
             // Needs to be a button or clickable element
             var restaurantName = $("<p>");
@@ -103,15 +81,10 @@ $(document).ready(function () {
             // In order to create API search for menu
         }
 
-    });
+    });    
 
 
-
-
-
-    
-
-
-
+    $(".modal").modal();
+    // Add event listener modal DECLINE or ACCEPT
 
 })
